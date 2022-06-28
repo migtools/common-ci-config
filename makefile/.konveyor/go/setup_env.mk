6da9b1dcac2b4e63ef7697ec5a93afe4c26a0839
Michal Pryc <mpryc@redhat.com>
2022-06-28 13:03:56 +0200

    Change go installation to extract all files and include go version
    Change to the install script as go requires all uncompressed
    files. Adds the pre-commit and go-version targets.
    
    In this installation methods go does not require to export
    GOROOT and relevant env variables as binary is within
    extracted dir.

--- makefile/.konveyor/go/setup_env.mk
+++ makefile/.konveyor/go/setup_env.mk
@@ -1,18 +1,9 @@
 ### Important, the LANG_DIR must match one from Makefile
 LANG_DIR=go
 
-GO_VENV:
-	mkdir .venv
-	mkdir .venv/bin
-
-$(LANG_DIR)-dev-env: GO_VENV
+# We require PYTHON_VENV for the pre-commit CLI
+$(LANG_DIR)-dev-env: $(PYTHON_VENV)
 	./$(COMMON_GIT_DIR)/makefile/scripts/install_dev_tools -v .venv -c go
-	export  PATH=$PATH:.venv/bin
-	export GOPATH="$HOME/go_projects"
-	export GOBIN="$GOPATH/bin"
-	export GOROOT=$HOME/go
-	export PATH=$PATH:$GOROOT/bin
-
 
 $(LANG_DIR)-clean-dev-env:
 	@echo "Cleaning up..."
