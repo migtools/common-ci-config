c0401d6d2c885ddb44908f1ed5e8f7890732b52b
Michal Pryc <mpryc@redhat.com>
2022-06-28 15:09:11 +0200

    Add go specific golangci-lint target to the pre-commit
    This is new golangci-lint target added to the pre-commit part
    of the Makefile for the go lang.

--- makefile/.konveyor/go/tests.mk
+++ makefile/.konveyor/go/tests.mk
@@ -7,6 +7,9 @@ $(LOCAL_LANG_DIR)-tests:
 pre-commit: setup-pre-commit
 	. ${PYTHON_VENV}/bin/activate && pre-commit
 
+golangci-lint: setup-pre-commit
+	. ${PYTHON_VENV}/bin/activate && pre-commit run golangci-lint
+
 go-version: setup-pre-commit $(LANG_DIR)-dev-env
 	. ${PYTHON_VENV}/bin/activate && \
 	go version
